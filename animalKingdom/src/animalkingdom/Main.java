aea3f30c41e52182ff4a3cc09f381b57932ffb00
Vicky <vyue013@gmail.com>
2019-09-04 15:20:23 -0700

    added tests for birds and fish

--- animalKingdom/src/animalkingdom/Main.java
+++ animalKingdom/src/animalkingdom/Main.java
@@ -18,7 +18,7 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println();
-        
+
         // call mammals - instantiate
         Mammals panda = new Mammals("Panda", 1869);
         Mammals zebra = new Mammals("Zebra", 1778);
@@ -39,6 +39,32 @@ public class Main
 
         System.out.println();
 
+        // call birds - instantiate
+        Birds pigeon = new Birds("Pigeon", 1837);
+        Birds peacock = new Birds("Peacock", 1821);
+        Birds toucan = new Birds("Toucan", 1758);
+        Birds parrot = new Birds("Parrot", 1824);
+        Birds swan = new Birds("Swan", 1758);
 
+        System.out.println("*** LIST OF BIRDS ***");
+        System.out.println("Name: " + pigeon.getName() + " Year Named: " + pigeon.getYear());
+        System.out.println("Name: " + peacock.getName() + " Year Named: " + peacock.getYear());
+        System.out.println("Name: " + toucan.getName() + " Year Named: " + toucan.getYear());
+        System.out.println("Name: " + parrot.getName() + " Year Named: " + parrot.getYear());
+        System.out.println("Name: " + swan.getName() + " Year Named: " + swan.getYear());
+
+        System.out.println();
+
+        // call fish - instantiate
+        Fish salmon = new Fish("Salmon", 1758);
+        Fish catfish = new Fish("Catfish", 1817);
+        Fish perch = new Fish("Perch", 1758);
+
+        System.out.println("*** LIST OF FISH ***");
+        System.out.println("Name: " + salmon.getName() + " Year Named: " + salmon.getYear());
+        System.out.println("Name: " + catfish.getName() + " Year Named: " + catfish.getYear());
+        System.out.println("Name: " + perch.getName() + " Year Named: " + perch.getYear());
+
+        System.out.println();
     }
 }
